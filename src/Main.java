f88fa8c805c938d698a209b441674db44a31253b
Андрей <volk.3894@yandex.ru>
2023-04-12 13:50:01 +0300

    add homework

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+// Необходимо реализовать метод разворота связного списка
+// (двухсвязного или односвязного на выбор).
+
+
+class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void reverse() {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+}
